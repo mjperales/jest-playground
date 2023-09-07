3ce6f227532e989b123706e3f0e1828a8ba18e82
Mayra Perales <5091158+mjperales@users.noreply.github.com>
2023-09-07 10:42:07 -0500

    refactor: add simple form to test

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,15 +1,42 @@
 import React from "react";
-import { InputText, Icon } from "@washingtonpost/wpds-ui-kit";
+import { InputText, Icon, styled, Button } from "@washingtonpost/wpds-ui-kit";
 import { Email } from "@washingtonpost/wpds-assets";
+
+const Wrapper = styled("div", {
+  display: "flex",
+  flexFlow: "column nowrap",
+  justifyContent: "space-around",
+  alignItems: "center",
+  alignContent: "center",
+});
+
+const FormStyled = styled("form", {
+  display: "flex",
+  flexDirection: "column",
+  gap: "$050",
+});
+
+const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+  console.log("submit");
+};
 
 export default function Form() {
   return (
-    <form onSubmit={() => console.log("submit")}>
-      <InputText id="email" icon="left" label="Email">
-        <Icon label="email">
-          <Email />
-        </Icon>
-      </InputText>
-    </form>
+    <Wrapper>
+      <h1>Example Form</h1>
+
+      <FormStyled onSubmit={handleOnSubmit}>
+        <InputText placeholder="Jane Doe" type="text" label="Name" />
+        <InputText id="email" icon="left" label="Email">
+          <Icon label="email">
+            <Email />
+          </Icon>
+        </InputText>
+        <Button variant="primary" icon="left">
+          Submit
+        </Button>
+      </FormStyled>
+    </Wrapper>
   );
 }
