5e557216b00ea35efe7c575f0a0de270a86f3cd1
Mayra Perales <5091158+mjperales@users.noreply.github.com>
2023-09-08 10:22:38 -0500

    chore: added more testing examples

--- pages/components/Form/Form.test.tsx
+++ pages/components/Form/Form.test.tsx
@@ -1,13 +1,19 @@
 /**
  * @jest-environment jsdom
  */
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Form from "./Form";
 
 describe("<Form />", () => {
   beforeAll(() => jest.clearAllMocks());
   it("Successfully renders component", () => {
-    render(<Form onSubmitHandler={() => console.log("submitted")} />);
+    render(
+      <Form
+        onSubmitHandler={(name, email) =>
+          console.log("submitted with:", name, email)
+        }
+      />
+    );
     const submitButton = screen.getByRole("button", { name: /submit/i });
     // missing name field
     // missing email field
@@ -16,4 +22,18 @@ describe("<Form />", () => {
   // it("Successfully updates the name field", () => {});
   // it("Successfully updates the email field", () => {});
   // it("Successfully calls handleOnSubmit", () => {});
+
+  //TODO: fix broken test
+  // it("Successfully clears inputs after submit", () => {
+  //   render(<Form onSubmitHandler={() => jest.fn} />);
+  //   fireEvent.change(screen.getByRole("textbox", { name: /name/i }), {
+  //     target: { value: "Mayra" },
+  //   });
+  //   fireEvent.change(screen.getByRole("textbox", { name: /email/i }), {
+  //     target: { value: "m.j.perales@gmail.com" },
+  //   });
+  //   fireEvent.click(screen.getByRole("button", { name: /submit/i }));
+  //   expect(screen.getByRole("textbox", { name: /name/i }).value).toBe("");
+  //   expect(screen.getByRole("textbox", { name: /email/i }).value).toBe("");
+  // });
 });
--- pages/form.tsx
+++ pages/form.tsx
@@ -2,5 +2,11 @@ import React from "react";
 import Form from "./components/Form/Form";
 
 export default function FormPage() {
-  return <Form onSubmitHandler={() => console.log("submitted")} />;
+  return (
+    <Form
+      onSubmitHandler={(name, email) =>
+        console.log("submitted with:", name, email)
+      }
+    />
+  );
 }
